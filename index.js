feab3f9e1f23a7009fb86476be0710fa67ff80b9
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-06-13 06:28:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = "lydia Nyamoita";
+
+const welcomeMessage = `Welcome to Flatbook,${currentUser}!`;
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+console.log(shortGreeting);  
